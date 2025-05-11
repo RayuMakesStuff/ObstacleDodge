8a5eedff9e1f5cdc6f8d1362c135713abf16ce58
RayuMakesStuff <51034418+RayuMakesStuff@users.noreply.github.com>
2025-05-11 21:40:01 +0200

    Working Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
         
+        transform.Translate(x * Time.deltaTime,  0, z * Time.deltaTime);
     }
 }
